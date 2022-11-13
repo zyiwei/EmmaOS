4a4f3a9264011ef512d153ef34da2ef2df762567
zyiwei <31623050+zyiwei@users.noreply.github.com>
2022-11-13 19:46:14 +0800

    Update sync.h

--- thread/sync.h
+++ thread/sync.h
@@ -10,12 +10,7 @@ struct semaphore {
    struct   list waiters;
 };
 
-/* 锁结构 */
-struct lock {
-   struct   task_struct* holder;	    // 锁的持有者
-   struct   semaphore semaphore;	    // 用二元信号量实现锁
-   uint32_t holder_repeat_nr;		    // 锁的持有者重复申请锁的次数
-};
+
 
 void sema_init(struct semaphore* psema, uint8_t value); 
 void sema_down(struct semaphore* psema);
